279cb0ea139c9d2ab761fef50797d444b800fd21
hami-r <mynameissaitama54@gmail.com>
2022-11-24 23:42:52 +0530

    all food component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AllFoodComponent } from './all-food/all-food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AllFoodComponent
   ],
   imports: [
     BrowserModule,
